904f635ba48f567c21bcf66f55e827ca86ad7fac
Keloran <keloran@nordicarts.net>
2016-02-27 22:22:07 +0000

    updated the build script

--- build.sh
+++ build.sh
@@ -1,5 +1,51 @@
 #!/bin/sh
 
-rm -rf Packages
-swift build -Xcc -I/usr/local/include/libbson-1.0
-sh .build/debug/Site
+usage()
+{
+cat << EOF
+  This script can clean and build
+
+  usage: $0 options
+
+  OPTIONS:
+    -h This message
+    -c Clean the packages
+EOF
+}
+
+deletePackages()
+{
+    rm -rf Packages
+    rm -rf .build
+}
+
+buildIt()
+{
+    swift build -Xcc -I/usr/local/include/libbson-1.0
+}
+
+runIt()
+{
+  if [ -f ".build/debug/Site" ]; then
+    .build/debug/Site
+  else
+    echo "File not built"
+  fi
+}
+
+while getopts ":hc" OPTION; do
+  case $OPTION in
+    h)
+      usage
+      exit
+      ;;
+    c)
+      deletePackages
+      ;;
+  esac
+done
+
+clear
+
+buildIt
+runIt
