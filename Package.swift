42fcd09280d6e296827d3339401aafbcc320a0b9
Keloran <keloran@nordicarts.net>
2016-02-28 23:33:24 +0000

    some slight updates

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ let package = Package(
     dependencies: [
         // Blackfish stuff
         .Package(url: "https://github.com/elliottminns/orca-mongodb", majorVersion: 0),
-        .Package(url: "https://github.com/elliottminns/blackfish", majorVersion: 0),
-
-        // 3rd party
-        .Package(url: "https://github.com/kylef/Stencil", majorVersion: 0),
+        .Package(url: "https://github.com/elliottminns/blackfish-stencil", majorVersion: 0),
     ]
 )
--- sources/main.swift
+++ sources/main.swift
@@ -13,6 +13,10 @@ app.use(renderer: StencilRenderer(), ext: ".mustache")
 app.use(path: "/", controller: IndexController())
 //app.use(path: "/test", controller: TestController())
 
+app.get("version") { request, response in
+    response.send(json: ["version": "1.0"])
+}
+
 // MARK: Listen
 app.listen(port: 80) { error in
   if error != nil {
